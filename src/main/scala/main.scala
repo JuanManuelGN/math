6cb423372dfebbe8edbe133602a4938c239a87f2
Juan Manuel García Navarro <juan.garcia@47deg.com>
2023-11-29 22:45:25 +0100

    Initial commit

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -18,42 +18,22 @@ def main(n: Long): Unit = {
     case x => loop(n * 3L + 1L, acc :+ x)
   }
 
-  type C = (Long, Int)
-  implicit object AgeOrdering extends Ordering[C] {
-    def compare(a: C, b: C): Int = a._2 compare b._2
-  }
-  type G = (Long, Long)
-  implicit object LOrdering extends Ordering[G] {
-    def compare(a: G, b: G): Int = a._1 compare b._1
+  type T = (Long, Long)
+  implicit object LOrdering extends Ordering[T] {
+    def compare(a: T, b: T): Int = a._1 compare b._1
   }
 
-  val resp: List[List[Long]] = (4L to n).toList.map(x => loop(x, List.empty[Long]))
+  val resp: List[List[Long]] = (1L to n).toList.map(x => loop(x, List.empty[Long]))
   val max = resp.flatten.max
-//  val mode =  {
-//    val grouped = resp.flatMap(_.dropRight(5)).groupBy(x => x)
-//    grouped.map{ case (k, ls) => (k, ls.size)}
-//      .max(AgeOrdering)._1
-//  }
-
-//List(
-//  List(5, 16, 8, 4, 2, 1),
-//  List(8, 4, 2, 1)
-//) -->
-//  List(
-//    (16, List(5, 16, 8, 4, 2, 1)),
-//    (8, List(8, 4, 2, 1))
-//  ) -->
-//    List(
-//      16/5,
-//      8/8
-//    )
+  val pathMax = resp.map(_.size).max
   val maxs = resp.map(ls => (ls.max, ls.head))
 
 
 
-//  print(s"Conjetura de Collatz para $n es ${resp.mkString("\n")}")
-  print(s"Número mayor de las progresiones = $max")
+//  print(s"Conjetura de Collatz para $n es \n${resp.mkString("\n")}")
+  print(s"Mayor número en una de las prograsiones = $max")
+  print(s"Camino mas largo = $pathMax")
 //  maxs.foreach(m => println(s"Ratio entre el mayor número de una progresión(${m._1}) y el inicio(${m._2}) = ${m._1 / m._2}"))
-  print(s"Mayor ratio = ${maxs.max(LOrdering) match {case(m, i) => m / i}}")
+  print(s"Mayor ratio entre el tamaño de una progresión y el número de inicio = ${maxs.max(LOrdering) match {case(m, i) => m / i}}")
 
 }
